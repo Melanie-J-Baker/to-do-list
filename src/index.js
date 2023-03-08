d098a3a7c600664ae74b9a00bcc9f0c7296d6ccf
Melanie-J-Baker <baker_mel@hotmail.com>
2023-03-08 16:12:38 +0000

    Write basic factory function to create to-dos and function to place them in nested array of projects

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
-import './style.css';
+import "./style.css";
+let projects = [];
+const newToDo = (name, desc, date, priority) => {
+  const sayHello = () => console.log("hello");
+  return { name, desc, date, priority, sayHello };
+};
+
+function addToProject() {
+  let project = [];
+  project.push(toDo);
+  projects.push(project);
+  console.log(projects);
+}
+
+let toDo = newToDo("washing", "ironing", "12/04/23", "High");
+addToProject(toDo);
