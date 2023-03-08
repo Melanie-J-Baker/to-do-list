4577ceb01f05381155e4dcdd00ab533b41216f4b
Melanie-J-Baker <baker_mel@hotmail.com>
2023-03-08 16:17:02 +0000

    Adjust basic addToProject function

--- src/index.js
+++ src/index.js
@@ -5,12 +5,13 @@ const newToDo = (name, desc, date, priority) => {
   return { name, desc, date, priority, sayHello };
 };
 
-function addToProject() {
+let toDo = newToDo("washing", "ironing", "12/04/23", "High");
+
+function addToProject(toDo) {
   let project = [];
   project.push(toDo);
   projects.push(project);
   console.log(projects);
 }
 
-let toDo = newToDo("washing", "ironing", "12/04/23", "High");
 addToProject(toDo);
