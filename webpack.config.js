7e40c3d3b98394c5eb80e297014786de3f3a87f9
Melanie-J-Baker <baker_mel@hotmail.com>
2023-09-13 18:02:31 +0100

    Remove HTMLWebpackPlugin and babel loader from webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,51 +1,31 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require("path");
 
 module.exports = {
-    entry: './src/index.js',
-    output: {
-        filename: 'main.js',
-        path: path.resolve(__dirname, 'dist'),
-        clean: true,
-    },
-    mode: 'development',
-    module: {
-        rules: [
-            { 
-                test: /\.txt$/, 
-                use: 'raw-loader' 
-            },
-            {
-                test: /\.css$/i,
-                use: ['style-loader', 'css-loader'],
-            },
-            {
-                test: /\.(png|svg|jpg|jpeg|gif)$/i,
-                type: 'asset/resource',
-            },
-            {
-                test: /\.(woff|woff2|eot|ttf|otf)$/i,
-                type: 'asset/resource',
-            },
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                    presets: ['@babel/preset-env']
-                    }
-                }
-            },
-        ],
-   
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            title: 'To-Do List',
-            template : path.join(__dirname, "src/template.html"),
-            filename : 'index.html',
-            inject : false
-        }),
+  entry: "./src/index.js",
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+  mode: "development",
+  module: {
+    rules: [
+      {
+        test: /\.txt$/,
+        use: "raw-loader",
+      },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: "asset/resource",
+      },
     ],
+  },
 };
